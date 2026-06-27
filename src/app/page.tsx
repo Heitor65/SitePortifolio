4f692cd83f863597eafddb97d02edc1efd1fc7a2
Heitor65 <multheitor@gmail.com>
2026-06-27 18:00:26 -0300

    page.tsx com a parte esquerda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
-import Navbar from "@/components/Navbar";
+import Left from "@/components/Left";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-
-      <h1>Minha página</h1>
+      <main className="flex min-h-screen flex-col md:flex-row">
+        <section className="flex flex-1 items-center justify-center p-8">
+          <Left />
+        </section>
+        <section className="flex flex-1 items-center justify-center p-8">
+          <p>is illo sequi a. Magni alias hic optio accusamus, cum pariatur atque molestias.</p>
+        </section>
+      </main>
     </>
   );
 }
