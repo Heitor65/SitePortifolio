83ffcceccc363083bf690a0540bfc35ffc37b7f0
Heitor65 <multheitor@gmail.com>
2026-06-27 19:29:32 -0300

    fix: duplicatas e erros de sintaxe

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Left() {
     return (
-        <nav className="flex flex-col gap-4 mt-0 mt-20 justify-items-end">
+        <nav className="flex flex-col gap-4 mt-20 justify-items-end">
             <span className="w-fit self-start hover:opacity-60 transition-opacity"><a href="#sobre">Sobre</a></span>
             <span className="w-fit self-start hover:opacity-60 transition-opacity"><a href="#experiencias">Experiencias</a></span>
             <span className="w-fit self-start hover:opacity-60 transition-opacity"><a href="#projetos">Projetos</a></span>
--- src/components/Redes.tsx
+++ src/components/Redes.tsx
@@ -2,10 +2,10 @@ export default function Redes() {
     return (
         <section className="flex gap-4 mt-25">
             <a href="https://github.com/Heitor65" target="_blank" rel="noopener noreferrer">
-                <img src="/images/github-brands-solid-full.svg" alt="GitHub" className="w-[50] h-[auto]" />
+                <img src="/images/github-brands-solid-full.svg" alt="GitHub" className="w-[50] h-auto" />
             </a>
             <a href="https://www.linkedin.com/in/heitor-g-lima" target="_blank" rel="noopener noreferrer">
-                <img src="/images/linkedin-brands-solid-full.svg" alt="LinkedIn" className="w-[50] h-[auto]" />
+                <img src="/images/linkedin-brands-solid-full.svg" alt="LinkedIn" className="w-[50] h-auto" />
             </a>
         </section>
     );
