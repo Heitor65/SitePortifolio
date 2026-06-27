0da3889d2970bdc1f5986454d9c10215ee00d5e2
Heitor65 <multheitor@gmail.com>
2026-06-27 19:29:09 -0300

    class name para scroll especial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Left from "@/components/Left";
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col md:flex-row">
-        <section className="flex flex-1 items-center justify-center p-8">
+      <main className="flex h-screen overflow-hidden">
+        <section className="w-1/2 flex items-center justify-center p-8 sticky top-0">
           <Left />
         </section>
-        <section className="flex flex-1 items-center justify-center p-8">
+        <section className="w-1/2 overflow-y-auto p-8">
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae cumque reprehenderit nesciunt reiciendis adipisci! Expedita, fuga vel quasi obcaecati architecto voluptatem voluptas eveniet facere atque ipsum necessitatibus provident, maxime iste!</p>
         </section>
       </main>
