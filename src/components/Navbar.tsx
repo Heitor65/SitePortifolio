04cfce6fb807c8819d279ecc291edb19dc02be5c
Heitor65 <multheitor@gmail.com>
2026-06-27 19:04:45 -0300

    opacidade no textinho do title e hover para a navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 export default function Left() {
     return (
         <nav className="flex flex-col gap-4 mt-0 mt-20 justify-items-end">
-            <span>Sobre</span>
-            <span>Experiencias</span>
-            <span>Projetos</span>
+            <span className="w-fit self-start hover:opacity-60 transition-opacity"><a href="#sobre">Sobre</a></span>
+            <span className="w-fit self-start hover:opacity-60 transition-opacity"><a href="#experiencias">Experiencias</a></span>
+            <span className="w-fit self-start hover:opacity-60 transition-opacity"><a href="#projetos">Projetos</a></span>
             <button className="mt-20 px-3 py-3 bg-[#4ccceb] text-white rounded-2xl hover:bg-[#4ccceb]/80 transition-colors">
                 Currículo
             </button>
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,7 @@ export default function Title() {
         <section className="flex flex-col items-start gap-3 mt-3 mb-10">
             <h1  className="text-5xl">Heitor Lima</h1>
             <h2 className="text-2xl">Desenvolvedor</h2>
-            <p className="ml-1 mt-3">Estudante de Engenharia da Computação</p>
+            <p className="ml-1 mt-3 opacity-60">Estudante de Engenharia da Computação</p>
         </section>
     );
 }
