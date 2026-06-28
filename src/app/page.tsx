e2d3ed6544dfe6963c055e14af08bc3a8944b033
Heitor65 <multheitor@gmail.com>
2026-06-28 12:37:41 -0300

    mudança de proporção e scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 import Left from "@/components/Left";
+import Right from "@/components/Right"
 
 export default function Home() {
   return (
     <>
-      <main className="flex h-screen overflow-hidden">
-        <section className="w-1/2 flex items-center justify-center p-8 sticky top-0">
-          <Left />
-        </section>
-        <section className="w-1/2 overflow-y-auto p-8">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae cumque reprehenderit nesciunt reiciendis adipisci! Expedita, fuga vel quasi obcaecati architecto voluptatem voluptas eveniet facere atque ipsum necessitatibus provident, maxime iste!</p>
-        </section>
+      <main className="h-screen overflow-hidden bg-zinc-950 px-4 py-4 sm:px-6 lg:px-2">
+        <div className="mx-auto flex h-full max-w-7xl flex-col gap-4 lg:flex-row lg:items-stretch">
+          <section className="flex flex-1 items-center justify-center p-8 sm:p-10 lg:h-full lg:flex-none lg:w-1/3 lg:sticky lg:top-4 lg:self-start">
+            <Left />
+          </section>
+          <section className="flex flex-1 flex-col justify-start overflow-y-auto scroll-smooth p-8 sm:p-10 lg:h-full lg:w-2/3">
+            <Right />
+          </section>
+        </div>
       </main>
     </>
   );
