9df1e42795009a528300005ff3a1bca376892106
Heitor65 <multheitor@gmail.com>
2026-06-30 18:57:12 -0300

    ajuste de responsividade das redes

--- src/components/Redes.tsx
+++ src/components/Redes.tsx
@@ -2,10 +2,10 @@ export default function Redes() {
     return (
         <section className="flex gap-4">
             <a href="https://github.com/Heitor65" target="_blank" rel="noopener noreferrer" className="float-icon rounded-full border border-zinc-800 bg-zinc-950/60 p-3 transition-colors hover:border-cyan-500/40 hover:bg-cyan-500/10">
-                <img src="/images/icons/github-brands-solid-full.svg" alt="GitHub" className="h-auto w-[50px]" />
+                <img src="/images/icons/github-brands-solid-full.svg" alt="GitHub" className="h-auto w-12.5" />
             </a>
             <a href="https://www.linkedin.com/in/heitor-g-lima" target="_blank" rel="noopener noreferrer" className="float-icon-delay rounded-full border border-zinc-800 bg-zinc-950/60 p-3 transition-colors hover:border-cyan-500/40 hover:bg-cyan-500/10">
-                <img src="/images/icons/linkedin-brands-solid-full.svg" alt="LinkedIn" className="h-auto w-[50px]" />
+                <img src="/images/icons/linkedin-brands-solid-full.svg" alt="LinkedIn" className="h-auto w-12.5" />
             </a>
         </section>
     );
