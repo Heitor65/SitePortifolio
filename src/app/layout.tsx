43af4b2cd3bcf527115600e51f74651fcb56844b
Heitor65 <multheitor@gmail.com>
2026-06-30 19:45:26 -0300

    fix: erro digitação no lang

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="ptBR"
+      lang="pt-BR"
       className={`${inter.variable} ${sora.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-zinc-950 text-zinc-100">
