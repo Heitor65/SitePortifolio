2085b37edb563a2be82ac61dddd8d853fe3aef72
Heitor65 <multheitor@gmail.com>
2026-07-01 16:14:03 -0300

    foto do projeto de API estagio

--- src/components/Portifolio.tsx
+++ src/components/Portifolio.tsx
@@ -4,7 +4,7 @@ export default function Portifolio() {
     return (
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
 
-            <Card title="API gestão de estágios" description="API REST para gestão de estágios, com autenticação por token, controle de acesso por perfis (Aluno, Secretaria e Coordenador), cadastro de documentação e fluxo de aprovação e reprovação de documentos." tech={["Python", "Django", "SQLite"]} repo="https://github.com/Projetos-de-Extensao/PBE_26.1_8002_IV.git" image="/images/cards/portifolio.png"
+            <Card title="API gestão de estágios" description="API REST para gestão de estágios, com autenticação por token, controle de acesso por perfis (Aluno, Secretaria e Coordenador), cadastro de documentação e fluxo de aprovação e reprovação de documentos." tech={["Python", "Django", "SQLite"]} repo="https://github.com/Projetos-de-Extensao/PBE_26.1_8002_IV.git" image="/images/cards/APIestagio.png"
             />
 
             <Card title="Site Portifólio" description="Site de portfólio pessoal criado para apresentar meus projetos, habilidades e experiências como desenvolvedor." tech={["TypeScript", "React", "Next.js", "Tailwind CSS"]} repo="https://github.com/Heitor65/SitePortifolio.git" image="/images/cards/portifolio.png"/>
