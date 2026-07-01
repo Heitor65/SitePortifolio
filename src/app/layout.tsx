b7996808cb26c42309f88d86d00a10fa13388180
Heitor65 <multheitor@gmail.com>
2026-07-01 15:31:08 -0300

    fix: tirando importação duplicada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Sora } from "next/font/google";
 import "./globals.css";
-import type { Metadata } from "next";
 
 const inter = Inter({
   variable: "--font-inter",
